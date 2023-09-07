6f7d4a3ddbe90a7c2baaf0b13f24ebc6c4666e9d
jolparjulian <jolparjulian@gmail.com>
2023-09-06 22:27:10 -0400

    9-6

--- Tests/Bruh.java
+++ Tests/Bruh.java
@@ -1,9 +1,19 @@
 package Tests;
+import java.io.*;
+import java.util.*;
 import javafx.application.*;
 import javafx.geometry.*;
 import javafx.scene.*;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.*;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.*;
 
 public class Bruh extends Application{
@@ -11,16 +21,7 @@ public class Bruh extends Application{
         Application.launch(args);
     }
     @Override
-    public void start(Stage primaryStage) {
-        TableView<String> tv = new TableView<>();
-        tv.setEditable(true);
-        TableColumn<String, String> emailCol = new TableColumn<>("email");
-        TableColumn<String, String> subjectCol = new TableColumn<>("subject");
-        TableColumn<String, String> timeCol = new TableColumn<>("times");
-        tv.getColumns().addAll(emailCol, subjectCol, timeCol);
-        VBox pane = new VBox(tv);
-        Scene scene = new Scene(pane, 500, 500);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    public void start(Stage stage) throws FileNotFoundException{
+        System.out.println("hi");
     }
 }
